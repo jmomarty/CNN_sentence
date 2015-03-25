6e1ea98df2ea3aeef1bac288eb0c46c4e9e3d19b
jmomarty <jmomarty@gmail.com>
2015-03-25 18:02:01 +0100

    data info

--- data_info.py
+++ data_info.py
@@ -10,9 +10,6 @@ parser = argparse.ArgumentParser(description='Data Stats')
 parser.add_argument('--input', default='mr.p')
 args = parser.parse_args()
 
-train_folder = args.train_files
-test_folder = args.test_files
-
 print "loading data...",
 x = cPickle.load(open(args.input,"rb"))
 revs, W, W2, word_idx_map, vocab = x[0], x[1], x[2], x[3], x[4]
@@ -21,7 +18,7 @@ pd_data_num_words = pd.DataFrame(revs)["num_words"]
 max_l = np.max(pd_data_num_words)
 mean_l = np.mean(pd_data_num_words)
 class_dist = pd.DataFrame(revs)["y"].values
-class_dist, _ = np.histogram(class_dist, bins = len(train_folder))
+class_dist, _ = np.histogram(class_dist, bins = 5)
 print "data loaded!"
 print "number of sentences: " + str(len(revs))
 print "vocab size: " + str(len(vocab))
