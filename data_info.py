e4cff75ca1152925f0f1ef2431c7ab8f7643dfe6
jmomarty <jmomarty@gmail.com>
2015-03-31 11:55:50 +0200

    train dev test

--- data_info.py
+++ data_info.py
@@ -8,7 +8,10 @@ import argparse
 # Arguments for the program:
 parser = argparse.ArgumentParser(description='Data Stats')
 parser.add_argument('--input', default='mr.p')
+parser.add_argument('--classes', default=2)
 args = parser.parse_args()
+
+classes = int(args.classes)
 
 print "loading data...",
 x = cPickle.load(open(args.input,"rb"))
@@ -18,10 +21,13 @@ pd_data_num_words = pd.DataFrame(revs)["num_words"]
 max_l = np.max(pd_data_num_words)
 mean_l = np.mean(pd_data_num_words)
 class_dist = pd.DataFrame(revs)["y"].values
-class_dist, _ = np.histogram(class_dist, bins = 5)
+class_dist, _ = np.histogram(class_dist, bins = classes)
+tdt_dist = pd.DataFrame(revs)["split"].values
+tdt_dist, _ = np.histogram(tdt_dist, bins = 3)
 print "data loaded!"
 print "number of sentences: " + str(len(revs))
 print "vocab size: " + str(len(vocab))
 print "max sentence length: " + str(max_l)
 print "average sentence length: " + str(mean_l)
 print "class distribution: " + str(class_dist)
+print "tdt distribution: " + str(tdt_dist)
