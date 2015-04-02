9a545a863713c0f3db73b2a373d2217ad297c148
jmomarty <jmomarty@gmail.com>
2015-04-02 17:06:33 +0200

    added one fully connected layer

--- conv_net_sentence.py
+++ conv_net_sentence.py
@@ -302,6 +302,7 @@ def make_idx_data_cv(revs, word_idx_map, cv, max_l=51, k=300, filter_h=5):
             test.append(sent)
         else:
             train.append(sent)
+    print train
     train = np.array(train)
     test = np.array(test)
     print train.shape, test.shape
