0de547fa2b455acbaedf57d2ff68d4a8d4777486
jmomarty <jmomarty@gmail.com>
2015-04-08 19:47:49 +0200

    changed activation function

--- process_data.py
+++ process_data.py
@@ -174,7 +174,7 @@ def load_bin_vec(fname, vocab):
                 if ch != '\n':
                     word.append(ch)   
             if word in vocab:
-                print word
+                # print word
                 word_vecs[word] = np.fromstring(f.read(binary_len), dtype='float32')
             else:
                 f.read(binary_len)
@@ -187,7 +187,7 @@ def add_unknown_words(word_vecs, vocab, min_df=1, k=300):
     """
     for word in vocab:
         if word not in word_vecs:
-            # print word
+            print word
             if vocab[word] >= min_df:
                 word_vecs[word] = np.random.uniform(-0.25,0.25,k)
 
