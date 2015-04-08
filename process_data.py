a811656705bde58b253dbfc44aa6e3e8815a7e94
jmomarty <jmomarty@gmail.com>
2015-04-08 18:19:58 +0200

    changed activation function

--- process_data.py
+++ process_data.py
@@ -186,6 +186,7 @@ def add_unknown_words(word_vecs, vocab, min_df=1, k=300):
     """
     for word in vocab:
         if word not in word_vecs and vocab[word] >= min_df:
+            print word
             word_vecs[word] = np.random.uniform(-0.25,0.25,k)  
 
 def clean_str(string, TREC=False):
