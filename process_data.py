7016c4b315d6a5ea5f2c5389753df11a1f9ef460
jmomarty <jmomarty@gmail.com>
2015-04-09 16:06:01 +0200

    changed activation function

--- process_data.py
+++ process_data.py
@@ -156,8 +156,6 @@ def load_bin_vec(fname, vocab):
     for word in w2v:
         if word or unidecode(word) in vocab:
             word_vecs[word] = w2v[word]
-        else:
-            print word
     return word_vecs
 
 def add_unknown_words(word_vecs, vocab, min_df=1, k=300):
