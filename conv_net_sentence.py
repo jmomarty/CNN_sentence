c622b65a1aafa160532184192346f882718d669b
jmomarty <jmomarty@gmail.com>
2015-04-10 12:46:34 +0200

    changed activation function

--- conv_net_sentence.py
+++ conv_net_sentence.py
@@ -81,7 +81,7 @@ def train_conv_net(datasets,
         filter_shape = filter_shapes[i]
         pool_size = pool_sizes[i]
         conv_layer = LeNetConvPoolLayer(rng, input=layer0_input,image_shape=(batch_size, 1, img_h, img_w),
-                                filter_shape=filter_shape, params_loaded= params_loaded, name_model = "cnet_"+i, poolsize=pool_size, non_linear=conv_non_linear)
+                                filter_shape=filter_shape, params_loaded= None, name_model = "cnet_"+i, poolsize=pool_size, non_linear=conv_non_linear)
         layer1_input = conv_layer.output.flatten(2)
         conv_layers.append(conv_layer)
         layer1_inputs.append(layer1_input)
