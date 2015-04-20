ef8dc0a7152a2aeb381d929123b8a7a90205c3df
jmomarty <jmomarty@gmail.com>
2015-04-20 16:52:37 +0200

    changed process_data

--- conv_net_sentence.py
+++ conv_net_sentence.py
@@ -69,8 +69,7 @@ def train_conv_net(dst,
     x = T.matrix('x')
     y = T.ivector('y')
     Words = theano.shared(value=np.asarray(wv, dtype=theano.config.floatX), name="Words")
-    layer0_input = Words[T.cast(x.flatten(),dtype="int32")].reshape((x.shape[0], 1, x.shape[1], Words.shape[1]))
-
+    layer0_input = Words[T.cast(x.flatten(),dtype="int32")]
     layer1 = HiddenLayer(rng,layer0_input, 300, 30, activation = None, use_bias = True)
     layer1_input = layer1.output.reshape((x.shape[0], 1, x.shape[1], 30))
 
