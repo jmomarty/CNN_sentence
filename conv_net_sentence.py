ee358afeb06712b463da759f5ef1354d04094927
jmomarty <jmomarty@gmail.com>
2015-04-26 23:44:32 +0200

    changed process_data

--- conv_net_sentence.py
+++ conv_net_sentence.py
@@ -319,8 +319,7 @@ def make_idx_data_cv(revs, mapping, cv, max_l=51, filter_h=5):
     for rev in revs:
         sent = get_idx_from_sent(rev["text"], mapping, rev["language"], max_l, filter_h)
         sent.append(rev["y"])
-        if len(sent) != 60:
-            continue
+        print len(sent)
         if rev["split"] == cv:
             print "OK"
             test.append(sent)
@@ -341,7 +340,7 @@ def make_idx_data_tdt(revs, mapping, max_l=51, filter_h=5):
     for rev in revs:
         sent = get_idx_from_sent(rev["text"], mapping, rev["language"], max_l, filter_h)
         sent.append(rev["y"])
-        print len(sent)
+
         if rev["split"]==1:
             dev.append(sent)
         if rev["split"]==2:
